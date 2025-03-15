a9e5fc2e3f19d9004860cdd6b56ab8e4fd393784
KMike226 <kaboremichee22@gmail.com>
2025-03-15 11:10:00 +0100

    feat(s3): partitioning intelligent du data lake par date/heure/symbole

--- infrastructure/terraform/environments/dev/main.tf
+++ infrastructure/terraform/environments/dev/main.tf
@@ -1,13 +1,4 @@
 # Configuration Terraform pour l'environnement de développement
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 # Configuration du provider AWS
 provider "aws" {
